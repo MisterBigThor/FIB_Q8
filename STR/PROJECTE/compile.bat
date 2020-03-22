f8dd17c2985f02853b6c50a1446be890bd19783f
greenTreeData <greentreedatasolutions@gmail.com>
2020-03-22 15:57:38 +0100

    str update

--- STR/PROJECTE/compile.bat
+++ STR/PROJECTE/compile.bat
@@ -1,5 +1,7 @@
-echo starting Compilation
+echo starting Compilation...
 
-arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c boot.S -o boot.o
-arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -std=gnu99 -c kernel.c -o kernel.o -O2 -Wall -Wextra
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c src\boot.S -o objects\boot.o
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -std=gnu99 -c src\kernel.c -o kernel.o -O2 -Wall -Wextra
 arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o
+
+echo end Compilation
